5804d6c67969a8857c88a5c811d9b4899ec5d9cf
Alan Ervey <alan@trueomni.com>
2023-07-25 20:20:23 -0700

    fix deploy

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './errorhandler/exception.filter';
+const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
